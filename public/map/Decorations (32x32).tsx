18002d0346c92228955ad2abdfe90a54c2b7ff30
Alexei Tyagunov <Tyagunov217@gmail.com>
2025-11-27 21:42:17 +0300

    final

--- public/map/Decorations (32x32).tsx
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Decorations (32x32)" tilewidth="32" tileheight="32" tilecount="42" columns="7">
- <properties>
-  <property name="h" value="192"/>
-  <property name="w" value="224"/>
- </properties>
- <image source="Decorations (32x32).png" width="224" height="192"/>
-</tileset>
